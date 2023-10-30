e92341177d9e2ead37fef52b57de14394c12fa76
René Weselowski <rene.weselowski@gmail.com>
2023-10-30 10:46:19 +0100

    tinkering with filehandlers and little refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 use std::{fs, io};
-use std::fs::OpenOptions;
-use std::io::{Read, Write};
 
 use clap::Parser;
 use indexmap::IndexSet;
@@ -17,26 +17,106 @@ struct Cli {
     #[arg(short, long, help = "trim whitespaces")]
     trim: bool,
 
-    #[arg(short, long, help = "rewrite existing destination file to remove duplicates")]
+    #[arg(
+        short,
+        long,
+        help = "rewrite existing destination file to remove duplicates"
+    )]
     rewrite: bool,
 
     #[arg(long, help = "do not write to file, only output what would be written")]
     dry_run: bool,
 
     #[arg(help = "destination file")]
-    path: String,
+    filepath: String,
 }
 
 fn main() -> io::Result<()> {
     let args = Cli::parse();
+    let filepath = &args.filepath;
 
+    let mut lines = match load_file(&args) {
+        Ok(value) => value,
+        Err(value) => Err(value)?,
+    };
+
+    if !args.dry_run && args.rewrite {
+        let mut f = OpenOptions::new()
+            .append(false)
+            .truncate(true)
+            .write(true)
+            .create(true)
+            .open(filepath)
+            .expect("failed to open file");
+
+        for line in lines.iter() {
+            writeln!(f, "{}", line).expect("failed to write to file");
+        }
+
+        drop(f);
+    }
+
+    let stdin_lines = io::stdin().lines();
+    for stdin_line in stdin_lines {
+        let stdin_line = stdin_line?;
+
+        if !is_newline(&args, &lines, &stdin_line) {
+            continue;
+        }
+
+        lines.insert(stdin_line.to_string());
+
+        if !args.quiet_mode {
+            println!("{}", stdin_line);
+        }
+
+        if !args.dry_run {
+            let mut f: Option<File> = None;
+
+            if f.is_none() {
+                f = Some(
+                    OpenOptions::new()
+                        .append(true)
+                        .write(true)
+                        .create(true)
+                        .open(filepath)
+                        .expect("failed to open file"),
+                );
+            }
+
+            let mut file = f.unwrap();
+            writeln!(file, "{}", stdin_line).expect("failed to write to file");
+
+            drop(file);
+        }
+    }
+
+    if args.sort && !args.dry_run {
+        let mut f = OpenOptions::new()
+            .append(false)
+            .write(true)
+            .create(false)
+            .open(&args.filepath)
+            .expect("failed to open file");
+
+        lines.sort_by(|a, b| natord::compare_ignore_case(a.as_str(), b.as_str()));
+
+        for line in lines.iter() {
+            writeln!(f, "{}", line)?;
+        }
+
+        drop(f);
+    }
+
+    Ok(())
+}
+
+fn load_file(args: &Cli) -> Result<IndexSet<String>, io::Error> {
     let mut lines = IndexSet::new();
-    let _file = fs::File::open(&args.path).is_ok();
-    match fs::read_to_string(&args.path) {
+    match fs::read_to_string(&args.filepath) {
         Ok(data) => {
             for line in data.lines() {
-                let line = if args.trim { line.trim() } else { line };
-                if line == "" {
+                if !is_newline(args, &lines, &line) {
                     continue;
                 }
 
@@ -45,63 +125,24 @@ fn main() -> io::Result<()> {
         }
         Err(err) if err.kind() != io::ErrorKind::NotFound => {
             eprintln!("failed to open _file for reading: {}", err);
+
             return Err(err);
         }
         _ => {}
     }
 
-    if !args.dry_run {
-        let mut f = OpenOptions::new()
-            .append(!args.rewrite)
-            .truncate(args.rewrite)
-            .write(true)
-            .create(true)
-            .open(&args.path)
-            .expect("failed to open file");
+    Ok(lines)
+}
 
-        if args.rewrite {
-            for line in lines.iter() {
-                writeln!(f, "{}", line).expect("failed to write to file");
-            }
-        }
-
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer)?;
-
-        for line in buffer.lines() {
-            let line = if args.trim { line.trim() } else { line };
-            if line == "" {
-                continue;
-            }
-
-            if lines.contains(line) {
-                continue;
-            }
-
-            lines.insert(line.to_string());
-
-            if !args.quiet_mode {
-                println!("{}", line);
-            }
-
-            writeln!(f, "{}", line).expect("failed to write to file");
-        }
+fn is_newline(args: &Cli, lines: &IndexSet<String>, line: &str) -> bool {
+    let line = if args.trim { line.trim() } else { line };
+    if line == "" {
+        return false;
     }
 
-    if args.sort {
-        let mut f = OpenOptions::new()
-            .append(false)
-            .write(true)
-            .create(false)
-            .open(&args.path)
-            .expect("failed to open file");
-
-        lines.sort_by(|a, b| natord::compare(a.as_str(), b.as_str()));
-
-        for line in lines.iter() {
-            writeln!(f, "{}", line)?;
-        }
+    if lines.contains(line) {
+        return false;
     }
 
-    Ok(())
+    return true;
 }
